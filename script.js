80df357fd2f007cd71fde6a58d564fa29766e495
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-09 08:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort((a, b) => a-b);
+	let close;
+	for(let i = 0; i<arr.length-2; i++){
+		let temp = arr[i] + twosum(arr, i+1, target-arr[i]);
+		if(Math.abs(temp) < diff){
+			close = temp;
+		}
+	}
+	return temp;
 }
 
 module.exports = threeSum;
