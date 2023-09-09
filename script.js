8c80e6162268976bbf5fb1b586e2da6d7fd0d7de
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-09 08:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+function twosum(arr, n, j, target){
+	let l = j;
+	let r = n-1;
+	let diff = Number.MAX_VALUE;
+	while(l<r){
+		let sum = arr[l] + arr[r];
+		if(sum==target) return sum;
+		if(sum<target){
+			if(Math.abs(sum-target)<diff){
+				diff = Math.abs(sum-target);
+				temp = sum;
+				l++;
+			}
+		}
+		else{
+			if(Math.abs(sum-target)<diff){
+				diff = Math.abs(sum-target);
+				temp = sum;
+				r++;
+			}
+		}
+	}
+	return sum;
+}
+
 function threeSum(arr, target) {
 // write your code here
 	arr.sort((a, b) => a-b);
 	let close;
-	for(let i = 0; i<arr.length-2; i++){
-		let temp = arr[i] + twosum(arr, i+1, target-arr[i]);
+	let n = arr.length;
+	let diff = Number.MAX_VALUE;
+	for(let i = 0; i<n-2; i++){
+		let temp = arr[i] + twosum(arr, n, i+1, target-arr[i]);
 		if(Math.abs(temp) < diff){
 			close = temp;
 		}
